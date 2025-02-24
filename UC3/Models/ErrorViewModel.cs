a8136302c4e01361dff4dbd902c32ee907c17e70
Sietse <sietsedrent@gmail.com>
2025-02-24 23:03:15 +0100

    Set-up

---
+++ UC3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UC3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
