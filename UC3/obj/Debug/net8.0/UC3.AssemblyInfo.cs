e5bd986dfc31ffa372aab476280ed66684a552a8
Sietse <sietsedrent@gmail.com>
2025-02-24 23:37:30 +0100

    Add TrackPage

--- UC3/obj/Debug/net8.0/UC3.AssemblyInfo.cs
+++ UC3/obj/Debug/net8.0/UC3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UC3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8136302c4e01361dff4dbd902c32ee907c17e70")]
 [assembly: System.Reflection.AssemblyProductAttribute("UC3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UC3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
