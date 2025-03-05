5ddb0e914f50c8fc4ff16ce017fee110c301f6f9
Sietse <sietsedrent@gmail.com>
2025-03-05 21:08:23 +0100

    API Added buttons/fields

--- UC3/obj/Debug/net8.0/UC3.AssemblyInfo.cs
+++ UC3/obj/Debug/net8.0/UC3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UC3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d071edf4f0b273a33f25771dd83c67336733100")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53dccda4fb6fc606fb9aa7c369357356f84f0e2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("UC3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UC3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
