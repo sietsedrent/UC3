7a7d843c96d555eb3e69f7fdbc419c39b8e5c2aa
Sietse <sietsedrent@gmail.com>
2025-03-06 16:01:13 +0100

    Identity implementation

--- MailAPI/obj/Debug/net8.0/MailAPI.AssemblyInfo.cs
+++ MailAPI/obj/Debug/net8.0/MailAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MailAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f408e3175a32e2d766f9af91656d117374ac72dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a625134efb8f8bf672adb8313df5bd4ead06c5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MailAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MailAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- UC3/obj/Debug/net8.0/UC3.AssemblyInfo.cs
+++ UC3/obj/Debug/net8.0/UC3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UC3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ec78dbca290bbc9631a484344ef2ca824fc2864")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a625134efb8f8bf672adb8313df5bd4ead06c5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("UC3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UC3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
