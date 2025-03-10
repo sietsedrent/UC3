dec0eba8e0466883763bd3c39d6ab1842961a358
Sietse <sietsedrent@gmail.com>
2025-03-10 13:00:57 +0100

    fix

--- UC3/obj/Debug/net8.0/UC3.AssemblyInfo.cs
+++ UC3/obj/Debug/net8.0/UC3.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UC3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8242947ccedbbb3e199c6a4c4ec5daa8f0d339ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7790976ac9f1e18a9a8655a1414577c3205a4dc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("UC3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UC3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
