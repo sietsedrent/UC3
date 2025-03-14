395ae50d3ee62ae2b778e8d5755ab0c64eb79b22
Sietse <sietsedrent@gmail.com>
2025-03-14 20:28:12 +0100

    Improved search page

--- UC3/obj/Debug/net8.0/UC3.AssemblyInfo.cs
+++ UC3/obj/Debug/net8.0/UC3.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UC3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22835e30a79844b923125956f013e25e2da55ca5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd3adb36763204e025b511c58e2afe0778e7a37")]
 [assembly: System.Reflection.AssemblyProductAttribute("UC3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UC3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
